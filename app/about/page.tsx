c3c4920b17e8f006007fca495366c67f10359a8a
SHAHZAIbSAHITO <sahitoshahzaib888@gmail.com>
2024-10-30 18:28:25 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <Link href="/">Go to Home Page</Link>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ app/header/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Header Page</h1>
+        <Link href="/">Go to Home Page</Link>
+      
+    </div>
+  )
+}
+
+export default Header
